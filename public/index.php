cbabe88a28b19d33ebe946b054110f284520cf7f
Artem Sinelnikov <artmail.su@gmail.com>
2018-03-18 11:45:11 +0300

    request/respons HTTP pre psr

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+use Framework\Http\RequestFactory;
+use Framework\Http\Response;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+### Initialization
+
+$request = RequestFactory::fromGlobals();
+
+### Action
+
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+
+$response = (new Response('Hello, ' . $name . '!'))
+    ->withHeader('X-Developer', 'ElisDN');
+
+### Sending
+
+header('HTTP/1.0 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+foreach ($response->getHeaders() as $name => $value) {
+    header($name . ':' . $value);
+}
+echo $response->getBody();
